3136904b41c9fd43edbdbda33a817ae9ce654e32
sumingwei <46164371+SuMingWei@users.noreply.github.com>
2020-12-13 05:24:05 +0800

    [modify] modify the method of generation to raise hit rate

--- generate.cpp
+++ generate.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 char alpha[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+vector<long long> temp;
 
 string randomString(){
     string tmp = "";
@@ -27,15 +28,28 @@ long long randomLongLong(){
     return temp;
 }
 
+long long get_half(){
+    int x = rand() % 2;
+    if(x == 0){
+        return randomLongLong();
+    }else{
+        int i = rand() % temp.size();
+        return temp.at(i);
+    }
+}
+
 void generate_put(int size){
     ofstream fout;
+    long long key;
     fout.open("only_put_"+ to_string(size) + ".input");
     for(int i=0;i<size;i++){
+        key = randomLongLong();
         if(i==size-1){
-            fout << "PUT " << randomLongLong() << " " << randomString();
+            fout << "PUT " << key << " " << randomString();
             break;    
         }
-        fout << "PUT " << randomLongLong() << " " << randomString() << "\n";
+        fout << "PUT " << key << " " << randomString() << "\n";
+        temp.push_back(key);
     }
     fout.close();
 }
@@ -45,10 +59,10 @@ void generate_get(int size){
     fout.open("only_get_"+ to_string(size) + ".input");
     for(int i=0;i<size;i++){
         if(i==size-1){
-            fout << "GET " << randomLongLong();
+            fout << "GET " << get_half();
             break;    
         }
-        fout << "GET " << randomLongLong() << "\n";
+        fout << "GET " << get_half() << "\n";
     }
     fout.close();
 }
@@ -62,21 +76,21 @@ void generate_all(int size){
         temp = rand() % 5;
         if(i==size-1){
             if(temp == 0 || temp == 3){
-                fout << "PUT " << randomLongLong() << " " << randomString();
+                fout << "PUT " << get_half() << " " << randomString();
             }else if(temp == 1 || temp == 4){
-                fout << "GET " << randomLongLong();
+                fout << "GET " << get_half();
             }else{
-                head = randomLongLong();
+                head = get_half();
                 fout << "SCAN " << head << " " << head + (rand()%40) + 1;
             }
             break;    
         }
         if(temp == 0 || temp == 3){
-            fout << "PUT " << randomLongLong() << " " << randomString() << "\n";
+            fout << "PUT " << get_half() << " " << randomString() << "\n";
         }else if(temp == 1 || temp == 4){
-            fout << "GET " << randomLongLong() << "\n";
+            fout << "GET " << get_half() << "\n";
         }else{
-            head = randomLongLong();
+            head = get_half();
             fout << "SCAN " << head << " " << head + (rand()%40) + 1 << "\n";
         }
     }
@@ -102,10 +116,10 @@ void generate_scan(int size){
 int main(int argc, char** argv){
     srand(time(NULL));
     
-    //generate_put(27500000);
-    //generate_get(175000000);
-    //generate_all(55000000);
-    generate_scan(100000000);
+    generate_put(6800000);
+    generate_get(42000000);
+    generate_all(13500000);
+    //generate_scan(100000000);
     return 0;
 
 }
