9854fa9da53d5b761c466663e22cbe9f4ae1680f
sumingwei <46164371+SuMingWei@users.noreply.github.com>
2020-12-13 05:17:58 +0800

    [modify] modify structure

---
+++ store_v4.cpp
@@ -0,0 +1,251 @@
+# include <iostream>
+# include <fstream>
+# include <cstdlib>
+# include <cmath>
+# include <vector>
+# include <map>
+# include <random>
+
+using namespace std;
+
+int TLB_MAX = 1000000;
+int BUFFER_MAX = 1000000; //256000000
+int ROM_NUM = 128;
+int PAGE_NUM = 512;
+
+int classify(long long key){
+    // spilt to 512 files
+    return (int)(key / pow(2,54));
+}
+
+string get_filename(string s){
+    vector<string> buffer;
+    string tmp;
+    int current = 0, next;
+    while(1){
+        next = s.find_first_of('/',current);
+        if(next != current){
+            tmp = s.substr(current,next-current);
+            if(tmp.size() != 0){
+                buffer.push_back(tmp);
+            }
+        }
+        if(next == string::npos){
+            break;
+        }
+        current = next + 1;
+    }
+    return buffer.at(buffer.size()-1);
+}
+
+// check whether the page is in page_list
+bool is_page_in_pagelist(int page, map<int,long long> &page_list){
+    map<int,long long>::iterator iter = page_list.find(page);
+    if(iter != page_list.end()){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+// output file
+void output_file(string &buffer,string filename){
+    fstream fout;
+    fout.open(filename + ".output",ios::app);
+    fout << buffer;
+    buffer.clear();
+    fout.close();
+}
+
+// update page from TLB
+void update(int page,vector<long long> &start_key, map <long long,string> &TLB,map<long long,string> &ROM,map<int,long long>&page_list){
+    // check whether the page is in ROM 
+    bool in_rom = is_page_in_pagelist(page,page_list);
+    // move all keys that in same page to file
+    // check whether TLB has key 
+    if(start_key.at(page) != -1){
+        fstream fout;
+        fout.open("./storage/page"+ to_string(page) +".txt",ios::app);
+        for(map<long long, string>::iterator iter = TLB.find(start_key.at(page));iter != TLB.end();){
+            if(classify(iter->first) == page){
+                fout << iter->first << " " << iter->second << "\n";
+                // update ROM
+                if(in_rom){
+                    ROM[iter->first] = iter->second;
+                }
+                TLB.erase(iter++);
+            }else if(classify(iter->first) > page){
+                break;
+            }
+        }
+        start_key.at(page) = -1;
+        fout.close();
+    } 
+}
+
+// release ROM
+void release_rom(map<long long,string> &ROM,map<int,long long>&page_list){
+    int num = rand() % ROM_NUM;
+    map<int,long long>::iterator list_iter = page_list.begin();
+    for(int i=0;i<num;i++){
+        list_iter++;
+    }
+    // erase page in ROM
+    int page = list_iter->first;
+    long long key = list_iter->second;
+    for(map<long long, string>::iterator it = ROM.find(key);it != ROM.end();){
+        if(classify(it->first) == page){
+            ROM.erase(it++);
+        }else if(classify(it->first) > page){
+            break;
+        }
+    }
+    // erase page in page_list
+    page_list.erase(list_iter); 
+}
+
+// put
+void put_tlb(long long key,string value, vector<long long> &start_key,map<long long,string> &TLB,map<long long,string> &ROM,map<int,long long>&page_list){
+    // put value
+    TLB[key] = value;
+    // update start_key
+    int page = classify(key);
+    if(key < start_key.at(page) || start_key.at(page) == -1){
+        start_key.at(page) = key;
+    }
+    // if TLB is full
+    if(TLB.size() >= TLB_MAX){
+        update(page,start_key,TLB,ROM,page_list);
+    }
+}
+
+// get
+void get_tlb(long long key, string &output_buffer, vector<long long> &start_key, map<long long,string> &TLB, string filename,map<long long,string> &ROM,map<int,long long>&page_list){
+    string finalValue = "EMPTY";
+    int page = classify(key);
+    // check TLB and check ROM
+    map<long long, string>::iterator iter1 = TLB.find(key);
+    map<long long, string>::iterator iter2 = ROM.find(key);
+    if(iter1 != TLB.end()){
+        finalValue = iter1->second;
+    }else if(is_page_in_pagelist(page,page_list)){ 
+        if(iter2 != ROM.end()){
+            finalValue = iter2->second;
+        }else{
+            finalValue = "EMPTY";
+        }
+    }else{
+        // the key is not in TLB or ROM
+        long long tempKey;
+        string tempValue;
+        ifstream fin;
+        fin.open("./storage/page" + to_string(page) + ".txt");
+        // the page is not empty
+        if(!fin.eof()){
+            // assign page list
+            page_list[page] = -1;
+            // check whether ROM is full
+            if(page_list.size() >  ROM_NUM){
+                release_rom(ROM,page_list);
+            }
+            // read page
+            while(fin >> tempKey){
+                fin >> tempValue;
+                if(tempKey == key){
+                    finalValue = tempValue;
+                }
+                // move to ROM
+                ROM[tempKey] = tempValue;
+                if(tempKey < page_list[page] || page_list[page] == -1){
+                    page_list[page] = tempKey;
+                }
+            }
+        }
+        fin.close();
+
+    }
+    output_buffer += finalValue + "\n";
+    //cout << output_buffer.size() << "\n";
+    // if buffer is full, then write file 
+    if(output_buffer.size() >= BUFFER_MAX){
+        output_file(output_buffer,filename);
+    }
+    
+}
+
+// scan
+void scan_tlb(long long key1, long long key2, string &output_buffer, vector<long long> &start_key,map<long long,string> &TLB, string filename,map<long long,string> &ROM,map<int,long long>&page_list){
+    for(long long i=key1;i<key2+1;i++){
+        get_tlb(i,output_buffer,start_key,TLB,filename,ROM,page_list);
+    }
+}
+
+void flush_tlb(map <long long,string> &TLB){
+    fstream fout;
+    for(int i=0;i<PAGE_NUM;i++){
+        fout.open("./storage/page"+ to_string(i) +".txt",ios::app);
+        for(map<long long, string>::iterator iter=TLB.begin();iter!=TLB.end();){
+            if(classify(iter->first) == i){
+                fout << iter->first << " " << iter->second << "\n";
+                TLB.erase(iter++);
+            }else{
+                break;
+            }
+        }
+        fout.close();
+        if(TLB.empty()){
+            break;
+        }
+    }
+}
+
+int main(int argc,char** argv){
+    string readString;
+    long long readKey;
+    long long readKey2;
+    string readValue;
+    string filename = "";
+    
+    system("mkdir ./storage");
+    srand(time(NULL));
+    // get file name
+    filename += argv[1];
+    filename = filename.substr(0,filename.length()-6);
+    filename = get_filename(filename);
+    // memory , max size == 1000000
+    map <long long,string> TLB;
+    string output_buffer = "";
+    map <long long,string> ROM;
+    // record ROM page (in time order)
+    map <int,long long> page_list; 
+    // record the first key of each page in TLB   
+    vector<long long> start_key;
+    for(int i=0;i<PAGE_NUM;i++){
+        start_key.push_back(-1);
+    }
+    // read input
+    ifstream fin;
+    fin.open(argv[1]);
+    while(fin >> readString){
+        fin >> readKey;
+        if(readString == "PUT"){
+            fin >> readValue;
+            put_tlb(readKey ,readValue ,start_key,TLB,ROM,page_list);
+        }else if(readString == "GET"){
+            get_tlb(readKey,output_buffer,start_key,TLB,filename,ROM,page_list);
+        }else if(readString == "SCAN"){
+            fin >> readKey2;
+            scan_tlb(readKey,readKey2,output_buffer,start_key,TLB,filename,ROM,page_list);
+        }
+    }
+    fin.close();
+    if(TLB.empty() == false){
+        flush_tlb(TLB);    
+    }
+    if(output_buffer.empty() == false){
+        output_file(output_buffer,filename);
+    }
+
+    return 0;
+}
+
